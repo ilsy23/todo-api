f5f7e0ff142b6714c47b2e55e39e018fa545aab4
ilsy23 <ilsysli@naver.com>
2023-11-24 10:26:40 +0900

    #1. 할 일 등록/목록 조회

--- src/main/java/com/example/todo/todoapi/service/TodoService.java
+++ src/main/java/com/example/todo/todoapi/service/TodoService.java
@@ -1,10 +1,18 @@
 package com.example.todo.todoapi.service;
 
+import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
+import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
+import com.example.todo.todoapi.entity.Todo;
 import com.example.todo.todoapi.repository.TodoRepository;
+import com.sun.xml.bind.v2.TODO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -13,5 +21,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class TodoService {
 
     private final TodoRepository todoRepository;
+
+    public TodoListResponseDTO create (final TodoCreateRequestDTO requestDTO)
+            throws RuntimeException {
+
+        todoRepository.save(requestDTO.toEntity());
+        log.info("할 일 저장 완료. 제목: {}", requestDTO.getTitle());
+
+        return retrieve();
+    }
+
+    // 글 전체 목록
+    public TodoListResponseDTO retrieve() {
+        List<Todo> entityList = todoRepository.findAll();
+
+        List<TodoDetailResponseDTO> dtoList = entityList.stream()
+                .map(TodoDetailResponseDTO::new)
+                .collect(Collectors.toList());
+
+        return TodoListResponseDTO.builder()
+                .todos(dtoList)
+                .build();
+    }
+
 
 }
